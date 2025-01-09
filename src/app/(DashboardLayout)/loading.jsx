f28427a0dba5e9cca5f512c2b0557cb1bfb00687
Chanuka-Gishen <76777641+Chanuka-Gishen@users.noreply.github.com>
2025-01-09 21:37:02 +0530

    fix(): updated set item values item controller

--- src/app/(DashboardLayout)/loading.jsx
+++ src/app/(DashboardLayout)/loading.jsx
@@ -1,7 +1,26 @@
-const Loading = () =>{
-    return(
-        <div>Dash Loading</div>
-    )
-}
+import { Box, CircularProgress, Typography } from "@mui/material";
+
+const Loading = () => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      gap={4}
+      sx={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        minWidth: "100%",
+        minHeight: "100%",
+      }}
+    >
+      <CircularProgress size={40} />
+      <Typography variant="h6">Loading..</Typography>
+    </Box>
+  );
+};
 
 export default Loading;
